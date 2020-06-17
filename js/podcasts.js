3c5d19d60bb40eb04f91968b59fa7bf0291d29ad
Alessandro1979-itac <muniz.caranha@gmail.com>
2020-06-17 11:20:25 -0400

    Adicionando algumas modificações nos arquivos

--- js/podcasts.js
+++ js/podcasts.js
@@ -1,3 +1,5 @@
+
+
 const realPodcasts = [
   {
     id: '88.5',
--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let inputRange = null;
 
 window.addEventListener('load', start);
 
-function start () {
+function start() {
   inputDisplay = document.querySelector('#inputDisplay');
   inputRange = document.querySelector('#inputRange');
   divPodcast = document.querySelector('.podcast');
@@ -13,19 +13,21 @@ function start () {
   renderPodcastFrom(inputRange.value);
 }
 
-function handleRangeInput (event) {
+function handleRangeInput(event) {
   const frequency = event.target.value;
   inputDisplay.value = frequency;
 
   renderPodcastFrom(frequency);
 }
 
-function renderPodcastFrom (frequency) {
+function renderPodcastFrom(frequency) {
   divPodcast.innerHTML = '';
 
   const podcast = realPodcasts.find((podcast) => podcast.id === frequency);
 
   if (!podcast) {
+    divPodcast.innerHTML = '<p>Nenhum podcast encontrado</p>';
+  } else {
     divPodcast.innerHTML = `
       <img class='podcastAvatar' src='./img/${podcast.img}' alt='${podcast.title}' />
       <h2>${podcast.title}</h2>
